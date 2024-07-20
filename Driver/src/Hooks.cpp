0da9b06406ee68cce4397ab2ad6dcbe61740f9c3
Pinwhell <60289470+pinwhell@users.noreply.github.com>
2024-07-20 04:19:12 -0400

    Hook Impls

--- Driver/src/Hooks.cpp
+++ Driver/src/Hooks.cpp
@@ -6,6 +6,8 @@
 #include <Kernel/UniSTD_x86.h>
 #include <Hooks.h>
 
+bool bStartupProcessed = false;
+
 bool StringEndWith(const char* str, const char* with)
 {
 	auto strLen = fslc_strlen(str);
@@ -55,24 +57,52 @@ long KERN_CALL hdo_sys_open(int dfd, const char __user* filename, int flags, uns
 	auto filenm = MemoryFromUserString<4096>(filename);
 
 	if (!CommCompare(procComm.c_str(), "com.example.hookme") ||
-		!(StringEndWith(filenm->c_str(), ".apk") && fslc_strstr(filenm->c_str(), "com.example.hookme")))
+		!(StringEndWith(filenm->c_str(), ".apk") && fslc_strstr(filenm->c_str(), "com.example.hookme")) ||
+		bStartupProcessed)
 		return fd;
 
 	// At this point, path is the wanted package .apk
+	// and the startup hasnt been processed yet
 	
 	auto threadComm = TaskCommGet(TaskCurrentGet());
 
 	KLOG_PRINT("%s:%s:Open -> %s", procComm.c_str(), threadComm.c_str(), filenm->c_str());
+	bStartupProcessed = true;
 
 	return fd;
+}
+
+REGPARAMDECL(void) __noreturn do_exit(long code);
+void (__noreturn KERN_CALL* odo_exit)(long code);
+void KERN_CALL hdo_exit(long code)
+{
+	Task proc = ProcessCurrentGet();
+	Task thread = TaskCurrentGet();
+
+	if (proc != thread)
+		return odo_exit(code);
+
+	auto comm = TaskCommGet(proc);
+
+	if(!CommCompare(comm.c_str(), "com.example.hookme"))
+		return odo_exit(code);
+
+	// At this point, the wanted Process is closing
+
+	KLOG_PRINT("%s:Close", comm.c_str());
+	bStartupProcessed = false;
+
+	odo_exit(code);
 }
 
 void SACHooksInstall()
 {
 	HOOKTRAMP_INSTALL(do_sys_open, hdo_sys_open, &odo_sys_open);
+	HOOKTRAMP_INSTALL(do_exit, hdo_exit, &odo_exit);
 }
 
 void SACHooksUninstall()
 {
+	HOOKTRAMP_RESTORE(do_exit, odo_exit);
 	HOOKTRAMP_RESTORE(do_sys_open, odo_sys_open);
 }
